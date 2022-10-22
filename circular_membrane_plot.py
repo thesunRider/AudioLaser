78459a854afa0a6cf0e3f1b2fdfa65e90a0a73a1
suryasaradhi <suryasaradhi3@gmail.com>
2022-10-22 16:14:30 +0530

    Added normals

--- circular_membrane_plot.py
+++ circular_membrane_plot.py
@@ -7,6 +7,10 @@ from functools import lru_cache
 import pandas as pd
 import scipy.optimize
 import scipy as sc
+from functools import partial
+from sklearn.neighbors import KDTree
+from mpl_toolkits.mplot3d import axes3d
+from sklearn.decomposition import PCA
 
 
 RADIUS = 1
@@ -23,7 +27,7 @@ all_cords = np.array([])
 FRAMES = len(MODES) * TIME_PER_MODE * FPS
 
 plotted_points = [None] * 5
-
+#np.seterr(divide='ignore', invalid='ignore')
 
 p0 = np.array([1, 1, 1]) # starting point for the line
 direction = np.array( np.sqrt([1/3, 1/3, 1/3])) # direction vector
@@ -35,7 +39,7 @@ def line_func(t):
     :returns      xyz-value as array"""
     return p0 + t*direction
 
-def target_func(t, time, m, n, RADIUS, SPEED_OF_SOUND):
+def opt_line(t, time, m, n, RADIUS, SPEED_OF_SOUND):
     """Function that will be minimized by fmin
     :param t:      curve parameter of the straight line
 
@@ -85,6 +89,16 @@ def get_vmin_vmax(m, n):
     vmax = np.max(jv(m, np.linspace(0, BESSEL_ROOTS[m][n], 100)))
     return -vmax, vmax
 
+def calc_cross(p1, p2, p3):
+    v1 = p2 - p1
+    v2 = p3 - p1
+    v3 =  np.cross(v1, v2)
+    return v3 / np.linalg.norm(v3)
+
+def PCA_unit_vector(array, pca=PCA(n_components=3)):
+    pca.fit(array)
+    eigenvalues = pca.explained_variance_
+    return pca.components_[ np.argmin(eigenvalues) ]
 
 def circular_membrane(r, theta, t, m, n, radius, speed_of_sound):
     l = lambda_mn(m, n, radius)
@@ -94,6 +108,10 @@ def circular_membrane(r, theta, t, m, n, radius, speed_of_sound):
     Theta = np.cos(m * theta )
 
     return R * T * Theta
+
+def calc_angle_with_xy(vectors):
+    l = np.sum(vectors[:,:2]**2, axis=1) ** 0.5
+    return np.arctan2(vectors[:, 2], l)
 
 
 r = np.linspace(0, RADIUS, 100)
@@ -152,20 +170,72 @@ def update(i, bar=None):
 
     if plotted_points[0] is not None:
         plotted_points[0][0].remove()
+        plotted_points[2][0].remove()
 
     if bar is not None:
         bar.update()
+
+    if plotted_points[1] is not None:
+        plotted_points[1].remove()
+
 
     t = i / FPS
     m, n = MODES[int(t // TIME_PER_MODE)]
 
     z = circular_membrane(r, theta, t, m, n, RADIUS, SPEED_OF_SOUND)
 
-    t_opt = sc.optimize.fmin(target_func, x0=-1,args=( t, m, n, RADIUS, SPEED_OF_SOUND))
+
+    #find normals of surface
+    X, Y, Z = map(lambda g: g.flatten(), [x, y, z])
+
+    #decrease points for plotting
+    iterations_remove = 3
+    for i in range(0,iterations_remove):
+        X = [am for i, am in enumerate(X) if i % 2 == 0]
+        Y = [am for i, am in enumerate(Y) if i % 2 == 0]
+        Z = [am for i, am in enumerate(Z) if i % 2 == 0]
+
+    data = np.array([X, Y, Z]).T
+    tree = KDTree(data, metric='minkowski') # minkowki is p2 (euclidean)
+    
+    dist, ind = tree.query(data, k=5) #k=3 points including itself
+    combinations = data[ind]
+    # map with functools
+    pca = PCA(n_components=3)
+    normals3 = list(map(partial(PCA_unit_vector, pca=pca), combinations))
+
+    kn = np.array(normals3)
+    kn[calc_angle_with_xy(kn) < 0] *= -1
+    u, v, w = kn.T
+
+    plotted_points[1] = ax.quiver(X, Y, Z, u, v, w, length=0.1, normalize=True,zorder=105)
+
+
+
+    #find intersection point
+    t_opt = sc.optimize.fmin(opt_line, x0=-1,args=( t, m, n, RADIUS, SPEED_OF_SOUND),disp=False,retall=False,full_output=False)
     intersection_point = line_func(t_opt)
 
-    print("line intersects at:",intersection_point)
     plotted_points[0] = ax.plot([intersection_point[0]],[intersection_point[1]],[intersection_point[2]], markerfacecolor='k', markeredgecolor='k', marker='o', markersize=5, alpha=0.9)
+
+
+    #find nearest normal
+    min_val = 10000000000
+    min_i = 0
+    for i in range(0,len(X)):
+        mod_val = np.linalg.norm(intersection_point - [X[i],Y[i],Z[i]])
+        if (mod_val < min_val):
+            min_val = mod_val
+            min_i = i
+
+    plotted_points[2] = ax.plot([X[min_i]],[Y[min_i]],[Z[min_i]], markerfacecolor='r', markeredgecolor='r', marker='o', markersize=5, alpha=0.9,zorder=103)
+
+    normal_direc = [u[min_i],v[min_i],w[min_i]]
+    normal_intersect = [X[min_i],Y[min_i],Z[min_i]]
+
+    print("line  at:",intersection_point)
+    print("Normal at:",normal_intersect)
+    print("Normal cords:",normal_direc)
 
     
     vmin, vmax = get_vmin_vmax(m, n)
